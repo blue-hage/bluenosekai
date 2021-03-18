900b2a4cacf116b0b5df91c37247abce12cb3f83
blue-hage <mahi.sakou.011047@gmail.com>
2021-03-19 01:36:46 +0900

    sentry code changed

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from flask import Flask
 import db, auth, blog, admin, contact
 
 sentry_sdk.init(
-    "https://e951567e5d324202929db2b21ffe7f21@o543738.ingest.sentry.io/5674088",
-    integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0
+    dsn="https://3312a0c0adb3482c85dc3e330a36dfca@o543738.ingest.sentry.io/5682771",
+    integrations=[FlaskIntegration()]
 )
 
 app = Flask(__name__)
