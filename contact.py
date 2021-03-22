6a1b359f7a8a5425a2a9b3023110de5a30e0858f
blue-hage <mahi.sakou.011047@gmail.com>
2021-03-22 19:39:16 +0900

    application form is almost there

--- contact.py
+++ contact.py
@@ -36,6 +36,8 @@ def contact():
             return render_template("contact-confirm.html", context=context)
         
         flash(error)
+        context={"name":name, "email":email, "body":body}
+        return render_template("contact-form.html", context=context)
         
     return render_template("contact-form.html")
 
@@ -46,9 +48,9 @@ def send_apply():
         email = request.form["email"]
         body = request.form["body"]
 
-        exec("INSERT INTO contact (name, email, body) VALUES (%s, %s, %s)", name, email, body)
+        # exec("INSERT INTO contact (name, email, body) VALUES (%s, %s, %s)", name, email, body)
         contact_create(email, name, body)
-        return redirect("/msg")
+        return redirect("/contact/msg/0")
     
     return render_template("contact-form.html")
 
@@ -60,6 +62,7 @@ def apply():
         email = request.form["email"]
         confirm = request.form["confirm"]
         tel = request.form["tel"]
+        which = request.form["which"]
         detail = request.form["detail"]
         budget = request.form["budget"]
         deadline = request.form["deadline"]
@@ -74,8 +77,10 @@ def apply():
             error = "メールアドレスが違います。"
         elif not tel:
             error = "電話番号を入力してください。"
-        elif not detail:
+        elif not which:
             error = "ご依頼内容を選択してください。"
+        elif not detail:
+            error = "ご依頼内容を入力してください。"
         elif not budget:
             error = "想定予算を入力してください。"
         elif not deadline:
@@ -83,32 +88,41 @@ def apply():
         elif not body:
             error = "目的・概要を入力してください。"
 
+
         if error is None:
-            context = {"name":name, "email":email, "tel":tel, "detail":detail, "budget":budget, "deadline":deadline, "body":body}
+            context = {"name":name, "email":email, "tel":tel, "which":which, "detail":detail, "budget":budget, "deadline":deadline, "body":body}
             return render_template("apply-confirm.html", context=context)
         
         flash(error)
+        context = {"name":name, "email":email, "tel":tel, "which":which, "detail":detail, "budget":budget, "deadline":deadline, "body":body}
+        return render_template("apply-form.html", context=context)
     
     return render_template("apply-form.html")
 
-@bp.route("/send_appllication", methods=["GET", "POST"])
-def send_appllication():
+@bp.route("/send_application", methods=["GET", "POST"])
+def send_application():
     if request.method == "POST":
         name = request.form["name"]
         email = request.form["email"]
         tel = request.form["tel"]
+        which = request.form["which"]
         detail = request.form["detail"]
         budget = request.form["budget"]
         deadline = request.form["deadline"]
         body = request.form["body"]
 
-        exec("INSERT INTO contact (name, email, tel, detail, budget, deadline, body) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
-        name, email, tel, detail, budget, deadline, body)
-        contact_create(email, name, body, tel, detail, budget, deadline)
-        return redirect("/msg")
+        # exec("INSERT INTO contact (name, email, tel, detail, budget, deadline, body) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
+        # name, email, tel, detail, budget, deadline, body)
+        contact_create(email, name, body, tel, which, detail, budget, deadline, body)
+        return redirect("/contact/msg/1")
     
     return render_template("apply-form.html")
 
-@bp.route("/msg")
-def msg():
-    return render_template("msg.html")
+@bp.route("/msg/<int:id>")
+def msg(id):
+    if id == 0:
+        return render_template("msg.html", title="お問い合わせ", sub="Contact Form", message="お問い合わせ")
+    elif id == 1:
+        return render_template("msg.html", title="制作依頼", sub="Request Form", message="制作依頼のお申し込み")
+    else:
+        return redirect("/")
